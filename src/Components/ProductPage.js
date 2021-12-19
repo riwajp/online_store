8f3e58380a8ee6b43aa77c5ec477360416926c51
riwajp <riwajfictious@gmail.com>
2021-12-19 13:30:10 +0545

    Update 1.0

--- src/Components/ProductPage.js
+++ src/Components/ProductPage.js
@@ -24,17 +24,39 @@ import { NotifiersAdd } from "../Actions";
 import { CartAdd } from "../Actions";
 import { CartToogle } from "../Actions";
 import { Link } from "react-router-dom";
+import { fetchProducts } from "../Actions";
+import { useEffect } from "react";
 
 const ProductPage = () => {
   const dispatch = useDispatch();
   const id = useParams().id;
   console.log(id);
   const all_products = useSelector((state) => state.productsFetch);
+
+  useEffect(() => {
+    fetch("https://electronic-ecommerce.herokuapp.com/api/v1/product")
+      .then((data) => data.json())
+      .then((data) => {
+        dispatch(fetchProducts(data));
+      });
+  });
+  if (all_products === "") {
+    fetch("https://electronic-ecommerce.herokuapp.com/api/v1/product")
+      .then((data) => data.json())
+      .then((data) => {
+        dispatch(fetchProducts(data));
+      });
+  }
+
   const cart = useSelector((state) => state.CartItems);
-  var product = all_products.data.product.filter(
-    (item) => item.id === parseInt(id)
-  );
-  product = product[0];
+  var product;
+  if (all_products !== "") {
+    product = all_products.data.product.filter(
+      (item) => item.id === parseInt(id)
+    );
+    product = product[0];
+  }
+
   console.log(product);
   var notifications = useSelector((state) => state.Notifiers);
 
@@ -75,65 +97,68 @@ const ProductPage = () => {
   }
   return (
     <ProductPageStyled>
-      {" "}
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={6}>
-          <Grid item md={5} sm={12}>
-            <Image
-              src={
-                "https://electronic-ecommerce.herokuapp.com/" + product.image
-              }
-            />
-          </Grid>
-          <Grid item md={6} sm={12}>
-            <Details>
-              <Category>
-                {product.category[0].substring(0, 1).toUpperCase() +
-                  product.category[0].substring(1)}{" "}
-                /{" "}
-                {product.category[1].substring(0, 1).toUpperCase() +
-                  product.category[1].substring(1)}
-              </Category>
-              <Name>{product.name}</Name>
-              <HL />
-              <br />
-              {product.stock > 0 ? (
-                <InStock>In Stock</InStock>
-              ) : (
-                <OutofStock>Out of Stock</OutofStock>
-              )}
-
-              <Description>
-                <Date>Date Added: {tsToDate(product.createDate)}</Date>
+      {all_products !== "" ? (
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={6}>
+            <Grid item md={5} sm={12}>
+              <Image
+                src={
+                  "https://electronic-ecommerce.herokuapp.com/" + product.image
+                }
+              />
+            </Grid>
+            <Grid item md={6} sm={12}>
+              <Details>
+                <Category>
+                  {product.category[0].substring(0, 1).toUpperCase() +
+                    product.category[0].substring(1)}{" "}
+                  /{" "}
+                  {product.category[1].substring(0, 1).toUpperCase() +
+                    product.category[1].substring(1)}
+                </Category>
+                <Name>{product.name}</Name>
+                <HL />
                 <br />
-                8GB DDR3 Toshiba RAM, 500GB SSD, Intel UHD Integrated Graphics,
-                15" Display
-              </Description>
-              <br />
-              <Price>
-                Rs.{" "}
-                {parseInt(product.price.substring(1) * 120).toLocaleString()}
-              </Price>
-              <br />
-              {product.stock > 0 ? (
-                <div>
-                  <AddToCart onClick={() => add_to_cart()}>
-                    Add to Cart
-                  </AddToCart>
-                  <Link to="/checkout" style={{ textDecoration: "none" }}>
-                    <BuyNow>Buy Now</BuyNow>
-                  </Link>
-                </div>
-              ) : (
-                <div>
-                  <ATCdisabled>Add to Cart</ATCdisabled>
-                  <BNdisabled>Buy Now</BNdisabled>
-                </div>
-              )}
-            </Details>
+                {product.stock > 0 ? (
+                  <InStock>In Stock</InStock>
+                ) : (
+                  <OutofStock>Out of Stock</OutofStock>
+                )}
+
+                <Description>
+                  <Date>Date Added: {tsToDate(product.createDate)}</Date>
+                  <br />
+                  8GB DDR3 Toshiba RAM, 500GB SSD, Intel UHD Integrated
+                  Graphics, 15" Display
+                </Description>
+                <br />
+                <Price>
+                  Rs.{" "}
+                  {parseInt(product.price.substring(1) * 120).toLocaleString()}
+                </Price>
+                <br />
+                {product.stock > 0 ? (
+                  <div>
+                    <AddToCart onClick={() => add_to_cart()}>
+                      Add to Cart
+                    </AddToCart>
+                    <Link to="/checkout" style={{ textDecoration: "none" }}>
+                      <BuyNow>Buy Now</BuyNow>
+                    </Link>
+                  </div>
+                ) : (
+                  <div>
+                    <ATCdisabled>Add to Cart</ATCdisabled>
+                    <BNdisabled>Buy Now</BNdisabled>
+                  </div>
+                )}
+              </Details>
+            </Grid>
           </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      ) : (
+        ""
+      )}{" "}
     </ProductPageStyled>
   );
 };
